2425adc5d4a9d6dfeeafe94042e266e51dd9227e
Fsp30 <depaulacostafilipe@gmail.com>
2025-04-25 19:38:13 -0300

    update for new module TypeProfessional

--- backend-maxidata/src/app.module.ts
+++ backend-maxidata/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeProfessionalModule } from './type-professional/type-professional.module';
 
 @Module({
-  imports: [],
+  imports: [TypeProfessionalModule],
   controllers: [AppController],
   providers: [AppService],
 })
