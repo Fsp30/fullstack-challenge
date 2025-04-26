1f18954b36d014d29b0c4dcd1920d1ffaa40bf12
Fsp30 <depaulacostafilipe@gmail.com>
2025-04-26 20:34:07 -0300

    create Profissional service: create

--- backend-maxidata/src/professional/professional.service.ts
+++ backend-maxidata/src/professional/professional.service.ts
@@ -1,11 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProfessionalDto } from './dto/create-professional.dto';
 import { UpdateProfessionalDto } from './dto/update-professional.dto';
+import { plainToInstance } from 'class-transformer'
+import { validateOrReject } from 'class-validator'
+import { PrismaClient, Professional } from '@generated/prisma'
+import { ResourceNotFoundException } from '@/shared/errors/ResourceNotFoundException';
 
 @Injectable()
 export class ProfessionalService {
-  create(createProfessionalDto: CreateProfessionalDto) {
-    return 'This action adds a new professional';
+  constructor(private prisma:PrismaClient){}
+
+  async create(input: CreateProfessionalDto){
+    const dto = plainToInstance(CreateProfessionalDto, input)
+
+    await validateOrReject(dto,{
+      whitelist:true,
+      forbidNonWhitelisted: true
+    })
+    const typeProfessional = await this.prisma.typeProfessional.findUnique({
+      where: {id: Number(dto.typeOfProfessionalId)}
+    })
+    if(!typeProfessional){
+      throw new ResourceNotFoundException("Tipo de Profissional")
+    }
+    return this.prisma.professional.create({
+      data: {
+        nome: dto.nome,
+        email: dto.email,
+        telephone: dto.telephone,
+        typeProfessionalId: dto.typeOfProfessionalId,
+        situation: dto.situation,
+      },
+    })
   }
 
   findAll() {
