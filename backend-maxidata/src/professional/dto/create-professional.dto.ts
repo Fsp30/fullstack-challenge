0129f0d5e3efb78e50f5155f56dc6e0f5ea462a5
Fsp30 <depaulacostafilipe@gmail.com>
2025-04-26 19:56:10 -0300

    Setup: Professional Dto

--- backend-maxidata/src/professional/dto/create-professional.dto.ts
+++ backend-maxidata/src/professional/dto/create-professional.dto.ts
@@ -1,1 +1,21 @@
-export class CreateProfessionalDto {}
+import { IsEmail, IsNotEmpty, IsString, IsInt, IsOptional, IsBoolean } from "class-validator";
+
+export class CreateProfessionalDto {
+        @IsString()
+        @IsNotEmpty()
+        name: string
+
+        @IsOptional()
+        @IsString()
+        telephone?:string
+
+        @IsOptional()
+        @IsEmail()
+        email?:string
+
+        @IsBoolean()
+        situation: boolean
+
+        @IsInt()
+        typeOfProfessionalId:number
+}
