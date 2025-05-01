e0af43e9189a4e58d02f52c0a967d82400049020
Fsp30 <depaulacostafilipe@gmail.com>
2025-04-30 22:14:49 -0300

    create: Google Login Button

---
+++ frontend-maxidata/src/components/shared/GoogleLoginButton.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+
+const ClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
+if (!ClientId) {
+  throw new Error("VITE_GOOGLE_CLIENT_ID undefined")
+}
+export function GoogleLoginButton() {
+  useEffect(() => {
+    if (window.google) {
+      window.google.accounts.id.initialize({
+        client_id: ClientId,
+        callback: handleCredentialResponse,
+      })
+
+      window.google.accounts.id.renderButton(
+        document.getElementById("google-button")!,
+        {
+          theme: "outline",
+          size: "large",
+        }
+      )
+    }
+  }, [])
+
+  const handleCredentialResponse = (
+    response: google.accounts.id.CredentialResponse
+  ) => {
+    console.log("ID Token: ", response.credential)
+    localStorage.setItem("auth_token", response.credential)
+    window.location.href = "/home"
+  }
+
+  return <div id="google-button"></div>
+}
