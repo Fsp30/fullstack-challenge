15f75e5f4be75b48014fd90d830de147a50d9800
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-02 18:49:44 -0300

    Resolution bug: 'Cannot find namespace JSX

--- frontend-maxidata/src/types/global.d.ts
+++ frontend-maxidata/src/types/global.d.ts
@@ -1,1 +1,10 @@
-/// <reference types="react" />
+import type React from "react";
+declare global {
+  namespace JSX {
+    interface Element extends React.ReactElement {}
+    interface ElementClass extends React.Component {}
+    interface IntrinsicElements {
+      [elemName: string]: any;
+    }
+  }
+}
