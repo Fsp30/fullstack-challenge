f57c6614fef72d217a6cd59ae15db1de01614446
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-02 19:50:40 -0300

    create ZodSchema from actions API

---
+++ frontend-maxidata/src/features/professionals/types.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod'
+
+const ProfessionalBaseSchema = z.object({
+        name: z.string().min(1, "Nome é obrigatório"),
+        telephone: z.string().min(11, "Telefone inválido"),
+        email: z.string().email('E-mail inválido'),
+        situation: z.boolean(),
+        typeOfProfessionalId: z.number().int().positive('Id do tipo do profissional inválido')
+})
+
+const CreateProfissionalSchema = ProfessionalBaseSchema
+const UpdateProfessionalSchema = ProfessionalBaseSchema
+
+const ProfessionalResponseSchema = ProfessionalBaseSchema.extend({
+        id: z.number(),
+        createdAt: z.string().datetime(),
+        updatedAt: z.string().datetime(),
+        typeProfessional: z.object({
+                id: z.number(),
+                describe: z.string(),
+                situation: z.boolean(),
+                createdAt: z.string().datetime(),
+                updatedAt: z.string().datetime(),
+        })
+})
+
+export type CreateProfessionalDto = z.infer<typeof CreateProfissionalSchema>
+export type UpdateProfessionaleDto = z.infer<typeof UpdateProfessionalSchema>
+export type ProfessionalResponse = z.infer<typeof ProfessionalResponseSchema>
