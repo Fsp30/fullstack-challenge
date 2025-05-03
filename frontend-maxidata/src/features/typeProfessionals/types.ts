2a0b463a399be269c7dd59793e9e63c7e217b17a
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-02 21:37:29 -0300

    create ZodSchema from TypeProfessionalService

---
+++ frontend-maxidata/src/features/typeProfessionals/types.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod'
+
+const TypeProfessionalBaseSchema = z.object({
+        describe: z.string().min(1, "Descrição é obrigatória"),
+        situation: z.boolean()
+})
+
+const CreateTypeProfessionalSchema = TypeProfessionalBaseSchema
+const UpdateTypeProfessionalSchema = TypeProfessionalBaseSchema
+
+const ResponseTypeProfessionalSchema = TypeProfessionalBaseSchema.extend({
+        id: z.number(),
+        describe: z.string(),
+        situation: z.string(),
+        createdAt: z.string().datetime(),
+        updatedAt: z.string().datetime(),
+})
+
+const TypeProfessionalListSchema = z.array(ResponseTypeProfessionalSchema)
+
+export type CreateTypeProfessionalDto = z.infer<typeof CreateTypeProfessionalSchema>
+export type UpdateTypeProfessionalDto = z.infer<typeof UpdateTypeProfessionalSchema>
+export type ResponseTypeProfessional = z.infer<typeof ResponseTypeProfessionalSchema>
+export type ResponseTypeProfessionalList = z.infer<typeof TypeProfessionalListSchema>
+ 
