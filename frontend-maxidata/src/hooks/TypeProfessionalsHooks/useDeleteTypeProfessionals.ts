81c8843cada7b9a9184b08345b95feaabc167288
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-09 20:13:34 -0300

    Create Hook: deleteTypeProfessional

---
+++ frontend-maxidata/src/hooks/TypeProfessionalsHooks/useDeleteTypeProfessionals.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteTypeProfessionals } from "../../features/typeProfessionals/api/deleteTypeProfessionals";
+
+type Params = {
+        id :string
+}
+
+export function useDeleteTypeProfessional() {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn: (id: Params) => deleteTypeProfessionals(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["typeProfessional"],
+      })
+    },
+  })
+
+  return {
+    data: mutation.data,
+    mutate: mutation.mutate,
+    isPending: mutation.isPending,
+    isError: mutation.isError,
+    isSuccess: mutation.isSuccess,
+    error: mutation.error,
+  }
+}
