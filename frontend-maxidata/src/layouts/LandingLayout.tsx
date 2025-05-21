c3f65014fefb31ad53c6ca17f4d3044806e889b9
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-21 20:56:36 -0300

    create base Layout for pages landing's

--- frontend-maxidata/src/layouts/LandingLayout.tsx
+++ frontend-maxidata/src/layouts/LandingLayout.tsx
@@ -1,10 +1,20 @@
 import { ReactNode } from "react";
+import { MenuPopover } from "../components/ui/MenuPopoverIndex";
 
-export function LandingLayout({ children }: {children: ReactNode}){
-        return (
-                <div className="min-h-screen text-neutral-300 flex-col flex items-center justify-center px-4">
-                        <header className="text-3x1">Professional Manager</header>
-                        <main className="w-full max-w-3x1">{children}</main>
+interface LandingLayoutProps {
+    children: ReactNode
+}
+
+export function LandingLayout({ children }: LandingLayoutProps) {
+    return (
+        <div className="min-h-screen bg-background text-foreground">
+            <header className="p-6 flex justify-between items-center border-b">
+                <h1 className="text-xl font-bold">Professional Manager</h1>
+                <div>  
+                    <MenuPopover/>
                 </div>
-        )
+            </header>
+            <main className="p-6">{children}</main>
+        </div>
+    )
 }
