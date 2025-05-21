931f6a5caf40b8e140d2e9d58ff6aef1b2d579a0
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-21 20:22:12 -0300

    Inject React Router

--- frontend-maxidata/src/App.tsx
+++ frontend-maxidata/src/App.tsx
@@ -1,10 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LoginPage } from './pages/Login'
+import { Home } from './pages/Home'
 import './styles/App.css'
 
 function App() {
   
   return (
-    <></>
+      <BrowserRouter>
+    <div>
+      <Routes>
+        <Route path="/Home" element={<Home/>} />
+        <Route path="/Login" element={<LoginPage/>} />
+      </Routes>
+      
+    </div>
+      
+    </BrowserRouter>
   )
 }
 
