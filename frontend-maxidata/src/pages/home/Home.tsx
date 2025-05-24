3afc5d193653a3bfac17aaaaa60b0d50680f2a80
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-24 20:35:15 -0300

    inject AppLayout in component Home

---
+++ frontend-maxidata/src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import { AppLayout } from "../../layouts/AppLayout";
+
+export function Home() {
+        return (
+                <AppLayout>
+
+                <div className="text-red-700">
+                        <p>ola</p>
+                </div>
+                </AppLayout>
+        )
+}
