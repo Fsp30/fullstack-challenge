5997b40707888b5453c815ba576196e0e5417a04
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-24 20:54:29 -0300

    insert space for aside with menu

--- frontend-maxidata/src/layouts/AppLayout.tsx
+++ frontend-maxidata/src/layouts/AppLayout.tsx
@@ -9,10 +9,11 @@ export function AppLayout({ children }: AppLayoutProps) {
                 <div className="h-screen overflow-hidden bg-background text-primary dark:bg-bgdark dark:text-textdark flex flex-col transition-colors duration-500">
                         <header className="p-6 flex justify-between items-center shrink-0">
                                 <h1 className="text-xl font-bold">Professional Manager</h1>
-                                <div>
-                                        <h3>menu</h3>
-                                </div>
+                                
                         </header>
+                        <aside className="w-8 gap-2 ml-4 p-6">
+                                <p>menu dropdown</p>
+                        </aside>
                         <main className="flex-1 flex items-center justify-center overflow-hidden">
                                 {children}
                         </main>
