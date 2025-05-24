c40944407d15098004a38cd6ffb3aa593ad6220e
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-24 20:12:16 -0300

    function export change

--- frontend-maxidata/src/pages/Login.tsx
+++ frontend-maxidata/src/pages/Login.tsx
@@ -1,9 +1,9 @@
 import { GoogleLoginButton } from "../components/shared/GoogleLoginButton";
 
-export default function LoginPage() {
+export function LoginPage() {
   return (
     <div className="h-screen flex items-center justify-center">
       <GoogleLoginButton />
     </div>
-  );
+  )
 }
