1ff9c1679acd07c8a60c8d497728a0de0b13bdae
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-28 20:29:05 -0300

    inject ToggleTheme button

--- frontend-maxidata/src/layouts/AppLayout.tsx
+++ frontend-maxidata/src/layouts/AppLayout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { ThemeToggle } from "../components/shared/ThemeToggle";
 
 interface AppLayoutProps {
         children: ReactNode
@@ -9,7 +10,10 @@ export function AppLayout({ children }: AppLayoutProps) {
                 <div className="h-screen overflow-hidden bg-background text-primary dark:bg-bgdark dark:text-textdark flex flex-col transition-colors duration-500">
                         <header className="p-6 flex justify-between items-center shrink-0">
                                 <h1 className="text-xl font-bold">Professional Manager</h1>
-                                
+                                <div className="">
+                                        <ThemeToggle/>
+                                        
+                                </div>
                         </header>
                         <aside className="w-8 gap-2 ml-4 p-6">
                                 <p>menu dropdown</p>
