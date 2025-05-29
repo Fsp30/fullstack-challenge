f54831d1dd229e293951cfb6d7806696c43d6ba7
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-28 21:26:48 -0300

    defining AppLayout pattern

--- frontend-maxidata/src/layouts/AppLayout.tsx
+++ frontend-maxidata/src/layouts/AppLayout.tsx
@@ -1,26 +1,33 @@
-import { ReactNode } from "react";
-import { ThemeToggle } from "../components/shared/ThemeToggle";
+import { ReactNode } from "react"
+import { ThemeToggle } from "../components/shared/ThemeToggle"
 
 interface AppLayoutProps {
-        children: ReactNode
+  children: ReactNode
 }
 
 export function AppLayout({ children }: AppLayoutProps) {
-        return (
-                <div className="h-screen overflow-hidden bg-background text-primary dark:bg-bgdark dark:text-textdark flex flex-col transition-colors duration-500">
-                        <header className="p-6 flex justify-between items-center shrink-0">
-                                <h1 className="text-xl font-bold">Professional Manager</h1>
-                                <div className="">
-                                        <ThemeToggle/>
-                                        
-                                </div>
-                        </header>
-                        <aside className="w-8 gap-2 ml-4 p-6">
-                                <p>menu dropdown</p>
-                        </aside>
-                        <main className="flex-1 flex items-center justify-center overflow-hidden">
-                                {children}
-                        </main>
-                </div>
-        )
+  return (
+    <div className="h-screen bg-background text-primary dark:bg-bgdark dark:text-textdark flex flex-col transition-colors duration-800">
+      
+      <header className="p-6 flex justify-between items-center bg-white dark:bg-bgdark shadow z-10">
+        <h1 className="text-xl font-bold">Professional Manager</h1>
+        <ThemeToggle />
+      </header>
+
+      <div className="flex flex-1 overflow-hidden">
+        <aside className="w-52 p-6 bg-zinc-100 dark:bg-bgdark shrink-0 overflow-y-auto border-r border-zinc-200 dark:border-zinc-800">
+          <div>
+                <p>menu dropdown</p>
+          </div>
+          <div>
+                <p>menu dropdown</p>
+          </div>
+        </aside>
+
+        <main className="flex-1 overflow-y-auto p-6 bg-white dark:bg-bgdark shadow-inner">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
 }
