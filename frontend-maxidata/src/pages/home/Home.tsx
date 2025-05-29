58605b84e5de8ec948455d60e893c52200f095c0
Fsp30 <depaulacostafilipe@gmail.com>
2025-05-28 21:29:56 -0300

    set basic page for layout

--- frontend-maxidata/src/pages/home/Home.tsx
+++ frontend-maxidata/src/pages/home/Home.tsx
@@ -1,12 +1,16 @@
-import { AppLayout } from "../../layouts/AppLayout";
+import { AppLayout } from "../../layouts/AppLayout"
 
 export function Home() {
-        return (
-                <AppLayout>
-
-                <div className="text-red-700">
-                        <p>ola</p>
-                </div>
-                </AppLayout>
-        )
+  return (
+    <AppLayout>
+      <div className="grid grid-cols-2 gap-4 dark:text-primary h-80">
+        <div className="bg-whiteCard dark:bg-zinc-800 p-4 rounded shadow ">
+          <p>eu</p>
+        </div>
+        <div className="bg-whiteCard dark:bg-gray-500 p-4 rounded shadow ">
+          <p>eu2</p>
+        </div>
+      </div>
+    </AppLayout>
+  )
 }
