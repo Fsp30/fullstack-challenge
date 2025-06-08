f4b3a510d55daef699d7517f3972fffb3b5216d2
Fsp30 <depaulacostafilipe@gmail.com>
2025-06-08 19:47:42 -0300

    add mock data fake for findAll Professionals

--- frontend-maxidata/src/features/professionals/services/ProfessionalService.ts
+++ frontend-maxidata/src/features/professionals/services/ProfessionalService.ts
@@ -3,33 +3,57 @@ import { CreateProfessionalDto } from "../types";
 import { UpdateProfessionalDto } from "../types";
 import { ProfessionalResponse } from "../types";
 import { ProfessionalListResponse } from "../types";
+import { fakeProfessionals } from "../../../test/mocks/fakeProfessionals";
 
 export const ProfessionalService = {
-        findAll: async(page:number = 1, limit:number = 10): Promise<{data: ProfessionalListResponse, total: number}> => {
-                const response = await api.get(`/professional?page=${page}&limit=${limit}`)
-                return response.data
-        },
+  findAll: async (
+    page: number = 1,
+    limit: number = 10
+  ): Promise<{ data: ProfessionalListResponse; total: number }> => {
+    if (import.meta.env.DEV) {
+      const start = (page - 1) * limit;
+      const end = start + limit;
+      const data = fakeProfessionals.slice(start, end);
 
-        findOne: async(id: number): Promise<ProfessionalResponse> => {
-                const response = await api.get(`/professional/${id}`)
-                return response.data
-        },
-        findByType: async(typeId: number, page:number = 1, limit:number = 10): Promise<{data: ProfessionalListResponse, total: number}> =>{
-                const response = await api.get(`/profeessional/filter/by-type/${typeId}&page=${page}&limit${limit}`)
-                return response.data
-        }
-        ,
-        create: async(data: CreateProfessionalDto): Promise<void> => {
-                await api.post("/professional", data)
-        },
+      return {
+        data,
+        total: fakeProfessionals.length,
+      }
+    }
 
-        update: async(data: UpdateProfessionalDto, id:number):Promise<ProfessionalResponse> =>{
-                const response = await api.put(`/professional/${id}`, data)
-                return response.data
-        },
+    const response = await api.get(`/professional?page=${page}&limit=${limit}`);
+    return response.data;
+  },
 
-        remove: async(id:number): Promise<void> =>{
-                await api.delete(`/professional/${id}`)
-        }
-        
-}
+  findOne: async (id: number): Promise<ProfessionalResponse> => {
+    const response = await api.get(`/professional/${id}`);
+    return response.data;
+  },
+
+  findByType: async (
+    typeId: number,
+    page: number = 1,
+    limit: number = 10
+  ): Promise<{ data: ProfessionalListResponse; total: number }> => {
+    const response = await api.get(
+      `/profeessional/filter/by-type/${typeId}&page=${page}&limit=${limit}`
+    );
+    return response.data;
+  },
+
+  create: async (data: CreateProfessionalDto): Promise<void> => {
+    await api.post("/professional", data);
+  },
+
+  update: async (
+    data: UpdateProfessionalDto,
+    id: number
+  ): Promise<ProfessionalResponse> => {
+    const response = await api.put(`/professional/${id}`, data);
+    return response.data;
+  },
+
+  remove: async (id: number): Promise<void> => {
+    await api.delete(`/professional/${id}`);
+  },
+};
