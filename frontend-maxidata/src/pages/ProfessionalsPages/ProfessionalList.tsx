56829ce96040318366571ad7d1c79d2bf973f1f7
Fsp30 <depaulacostafilipe@gmail.com>
2025-06-11 22:03:04 -0300

    .

--- frontend-maxidata/src/pages/ProfessionalsPages/ProfessionalList.tsx
+++ frontend-maxidata/src/pages/ProfessionalsPages/ProfessionalList.tsx
@@ -1,11 +1,12 @@
 import { AppLayout } from "../../layouts/AppLayout"
 import { ProfessionalsList } from "../../features/professionals/components/ProfessionalList"
 
+
 export function ProfessionalList() {
 
   return (
     <AppLayout>
-      <ProfessionalsList/>
+        <ProfessionalsList />
     </AppLayout>
   )
 }
