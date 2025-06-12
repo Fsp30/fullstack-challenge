8d0e81df914ad525bf7d7c8918a1bbf3b8cf80d2
Fsp30 <depaulacostafilipe@gmail.com>
2025-06-11 21:34:57 -0300

    create page UpdateSelector

---
+++ frontend-maxidata/src/pages/ProfessionalsPages/UpdateSelector.tsx
@@ -0,0 +1,10 @@
+import { UpdateSelectorComponent } from "../../features/professionals/components/UpdateSelectorComponent";
+import { AppLayout } from "../../layouts/AppLayout";
+
+export function UpdateSelectorProfessionals(){
+        return(
+                <AppLayout>
+                        <UpdateSelectorComponent/>
+                </AppLayout>
+        )
+}
