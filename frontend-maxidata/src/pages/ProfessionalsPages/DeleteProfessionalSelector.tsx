0a627ace118dab69eb254b89ae6e54b4f876bcac
Fsp30 <depaulacostafilipe@gmail.com>
2025-06-13 20:33:29 -0300

    create Page DeleteSelectorProfessional Page

---
+++ frontend-maxidata/src/pages/ProfessionalsPages/DeleteProfessionalSelector.tsx
@@ -0,0 +1,10 @@
+import { DeleteSelectorComponent } from "../../features/professionals/components/DeleteSelectorComponent";
+import { AppLayout } from "../../layouts/AppLayout";
+
+export function DeleteSelectorProfessional(){
+        return(
+                <AppLayout>
+                        <DeleteSelectorComponent/>
+                </AppLayout>
+        )
+}
