048b6e7173485cbaf2729841b0ac73b796a8050a
Fsp30 <depaulacostafilipe@gmail.com>
2025-06-13 18:53:58 -0300

    route inject ProfessionalUpdate

--- frontend-maxidata/src/routes/Professionals.routes.tsx
+++ frontend-maxidata/src/routes/Professionals.routes.tsx
@@ -2,12 +2,14 @@ import { Route } from 'react-router-dom'
 import { ProfessionalsPage } from '../pages/ProfessionalsPages'
 import { ProfessionalList } from '../pages/ProfessionalsPages/ProfessionalList'
 import { UpdateSelectorProfessionals } from '../pages/ProfessionalsPages/UpdateSelector'
+import { ProfessionalFormUpdate } from '../pages/ProfessionalsPages/UpdateProfessionalsForm'
 
 export function ProfessionalsRoutes(){
         return( <>
                 <Route path="/professionals" element={<ProfessionalsPage/>}/>
                 <Route path="/professionals/findAll" element={<ProfessionalList/>}/>
                 <Route path="/professionals/update" element={<UpdateSelectorProfessionals/>}/>
+                <Route path="/professionals/update/:id" element={<ProfessionalFormUpdate/>}/>
                 </>
         )
 }
