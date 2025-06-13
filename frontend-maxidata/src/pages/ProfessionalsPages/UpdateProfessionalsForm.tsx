9adae5e3de319da2e1edff86e3ca5a5fc25cf0b7
Fsp30 <depaulacostafilipe@gmail.com>
2025-06-13 18:52:23 -0300

    create page ProfessionalFormUpdate

---
+++ frontend-maxidata/src/pages/ProfessionalsPages/UpdateProfessionalsForm.tsx
@@ -0,0 +1,10 @@
+import { UpdateProfessionalForm } from "../../features/professionals/components/UpdateProfessionalForm";
+import { AppLayout } from "../../layouts/AppLayout";
+
+export function ProfessionalFormUpdate(){
+        return(
+                <AppLayout>
+                        <UpdateProfessionalForm/>
+                </AppLayout>
+        )
+}
